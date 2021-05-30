0fe970f21600eb0cbf2e638531a4eae1a098dd41
maksymsoshyn <maksym.soshyn@protonmail.com>
2021-05-30 20:30:29 +0300

    Project init

---
+++ src/main/java/touk/ticket/booking/toukticketbooking/ToukTicketBookingApplication.java
@@ -0,0 +1,13 @@
+package touk.ticket.booking.toukticketbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToukTicketBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToukTicketBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/touk/ticket/booking/toukticketbooking/ToukTicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package touk.ticket.booking.toukticketbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToukTicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
